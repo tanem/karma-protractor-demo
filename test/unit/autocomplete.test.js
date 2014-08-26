069bc7cbd2f6e35567b7425dba7d733866b52bef
tanem <tane.morgan@gmail.com>
2014-08-26 15:40:52 +1200

    Decruft

--- test/unit/autocomplete.test.js
+++
@@ -1,9 +0,0 @@
-var Autocomplete = require('../../src/autocomplete');
-
-describe('Autocomplete', function(){
-
-  it('blah blah blah', function(){
-    // console.log(new Autocomplete({ data: [] }));
-  }); 
-
-});
